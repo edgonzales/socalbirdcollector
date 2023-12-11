b89f21e785cf2cd53d750afeb356230e4496067b
Ed Gonzales <edgonzales6@gmail.com>
2023-12-11 15:31:16 -0800

    Add Bird Detail Page

--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
+from .models import Bird
 
 # views.py
 
 # Add this cats list below the imports
-birds = [
-  {'name': 'Novie', 'breed': 'turkey vulture', 'description': 'furry little demon', 'age': 3},
-  {'name': 'Woody', 'breed': 'acorn woodpecker', 'description': 'gentle and loving', 'age': 2},
-]
+# birds = [
+#   {'name': 'Novie', 'breed': 'turkey vulture', 'description': 'furry little demon', 'age': 3},
+#   {'name': 'Woody', 'breed': 'acorn woodpecker', 'description': 'gentle and loving', 'age': 2},
+# ]
 
 
 # Create your views here.
@@ -19,6 +20,9 @@ def about(request):
 # Add new view
 def birds_index(request):
   # We pass data to a template very much like we did in Express!
-  return render(request, 'birds/index.html', {
-    'birds': birds
-  })
+  birds = Bird.objects.all()
+  return render(request, 'birds/index.html', { 'birds': birds })
+
+def birds_detail(request, bird_id):
+  bird = Bird.objects.get(id=bird_id)
+  return render(request, 'birds/detail.html', {'bird': bird})
