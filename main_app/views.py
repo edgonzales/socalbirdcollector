ce116ce3650e9e216c20d42f8b78df8638e7537d
Ed Gonzales <edgonzales6@gmail.com>
2023-12-11 11:00:40 -0800

    Add About and View All My Birds

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
+# views.py
+
+# Add this cats list below the imports
+birds = [
+  {'name': 'Novie', 'breed': 'turkey vulture', 'description': 'furry little demon', 'age': 3},
+  {'name': 'Woody', 'breed': 'acorn woodpecker', 'description': 'gentle and loving', 'age': 2},
+]
+
+
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+# Add new view
+def birds_index(request):
+  # We pass data to a template very much like we did in Express!
+  return render(request, 'birds/index.html', {
+    'birds': birds
+  })
