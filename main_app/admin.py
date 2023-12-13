551ba97b0247f5e80f1d2935568c297a2e2bf075
Ed Gonzales <edgonzales6@gmail.com>
2023-12-13 10:40:32 -0800

    Display feedings in Detail page

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,8 @@
 # Register your models here.
 from django.contrib import admin
 # import your models here
-from .models import Bird
+from .models import Bird, Feeding
 
 # Register your models here
 admin.site.register(Bird)
+admin.site.register(Feeding)
