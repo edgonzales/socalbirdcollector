d4196ca641e0664b65ac910937f5ead2dd08a7e0
Ed Gonzales <edgonzales6@gmail.com>
2023-12-14 09:18:35 -0800

    Include House and Photo to the admin.py

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,8 +1,10 @@
 # Register your models here.
 from django.contrib import admin
 # import your models here
-from .models import Bird, Feeding
+from .models import Bird, Feeding, House, Photo
 
 # Register your models here
 admin.site.register(Bird)
 admin.site.register(Feeding)
+admin.site.register(House)
+admin.site.register(Photo)
